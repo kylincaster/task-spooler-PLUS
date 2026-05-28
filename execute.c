447cc0e86fada89619f1ca2a5746c702842bf649
kylincaster <12872755+kylincaster@user.noreply.gitee.com>
2026-05-28 21:14:08 +0800

    add freezer_wait

--- execute.c
+++ execute.c
@@ -32,9 +32,7 @@
 
 
 
-#ifndef CGROUP_V2
 int cgroups_freezer_ok(int jobid, pid_t pid);
-#endif
 
 /*
 static int wait_for_pid(int pid)
@@ -367,12 +365,10 @@ static void run_child(int fd_send_filename, const char *tmpdir, int jobid) {
        kill -- -`ts -p` */
   setsid();
 
-#ifndef CGROUP_V2
   pid_t pid = getpid();
   while(cgroups_freezer_ok(jobid, pid) != 1) {
     usleep(30000);
   }
-#endif
   // only execute the command without the relink flag
   execvp(command_line.command.array[0], command_line.command.array);
 }
